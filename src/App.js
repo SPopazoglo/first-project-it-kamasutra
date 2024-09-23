35f6c2ac3de028c48b80eef15a588ee6b7cee082
SPopazoglo <sergiiipopazoglo@gmail.com>
2024-09-23 15:48:48 +0300

    Add new components: Music, News, Settings. Created routing on client side

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import Navbar from './components/navbar/Navbar'
 import Profile from './components/profile/Profile'
 import Dialogs from './components/Dialogs/Dialogs'
+import News from './components/News/News'
+import Music from './components/Music/Music'
+import Settings from './components/Settings/Settings'
 import './App.css'
 
 function App() {
   return (
-    <div className="App app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="App app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="profile" element={<Profile />} />
+            <Route path="dialogs" element={<Dialogs />} />
+            <Route path="news" element={<News />} />
+            <Route path="music" element={<Music />} />
+            <Route path="settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="profile">Profile</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="dialogs">Messages</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/news">News</a>
+        <NavLink to="news">News</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/music">Music</a>
+        <NavLink to="music">Music</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="settings">Settings</NavLink>
       </div>
     </nav>
   )
