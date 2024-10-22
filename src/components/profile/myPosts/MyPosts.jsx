1818a8529c88577448b0669f76e3dcdd7750832d
SPopazoglo <sergiiipopazoglo@gmail.com>
2024-10-22 13:15:49 +0300

    Lesson 31. createRef in MyPosts.jsx

--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -3,15 +3,22 @@ import Post from './post/Post'
 import styles from './MyPosts.module.css'
 
 function MyPosts(props) {
+  let newPostElement = React.createRef()
+
+  const addPost = () => {
+    let text = newPostElement.current.value
+    alert(text)
+  }
+
   return (
     <div className={styles.postsBlock}>
       <h3>My Posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>add post</button>
+          <button onClick={addPost}>add post</button>
         </div>
       </div>
       <div className={styles.posts}>
