aeadb8f762495c7591bedf59e6ec55ae19315194
SPopazoglo <sergiiipopazoglo@gmail.com>
2024-10-30 15:34:36 +0200

    lesson 35. observer pattern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import state from './redux/state'
-import { rerenderEntireTree } from './render'
+import state, { subscribe } from './redux/state'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { addPost, updateNewPostText } from './redux/state'
 import './index.css'
 
+let rerenderEntireTree = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <BrowserRouter>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+      />
+    </BrowserRouter>
+  )
+}
+
 rerenderEntireTree(state)
+
+subscribe(rerenderEntireTree)
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from '../render'
+let rerenderEntireTree = () => {
+  console.log('state changed')
+}
 
 let state = {
   profilePage: {
@@ -28,7 +30,7 @@ let state = {
   },
 }
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -36,12 +38,16 @@ export let addPost = () => {
   }
   state.profilePage.postsData.push(newPost)
   state.profilePage.newPostText = ''
-  rerenderEntireTree(state)
+  rerenderEntireTree()
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText
-  rerenderEntireTree(state)
+  rerenderEntireTree()
+}
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer // наблюдатель это паттерн
 }
 
 export default state
