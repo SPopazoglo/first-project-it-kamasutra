e8fb8773ddd33f5f917ae1861010c25a7ea09557
SPopazoglo <sergiiipopazoglo@gmail.com>
2024-12-04 15:14:09 +0200

    Rename from state.js to store.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import store from './redux/state'
+import store from './redux/store'
 import './index.css'
 
 let rerenderEntireTree = (state) => {
