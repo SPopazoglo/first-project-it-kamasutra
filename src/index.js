036e6c4dbc8422d7f11b3a5e8f4aa72edcd03c90
SPopazoglo <sergiiipopazoglo@gmail.com>
2024-12-09 14:43:20 +0200

    Lesson 45. connect, mapStateToProps, mapDispatchToProps

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import store from './redux/reduxStore'
 import './index.css'
-import { Provider } from './StoreContext'
 
 let rerenderEntireTree = () => {
   const root = ReactDOM.createRoot(document.getElementById('root'))
