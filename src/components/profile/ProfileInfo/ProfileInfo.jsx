2ab27196dd456b8ac7f42560c9fd09ac42711580
SPopazoglo <sergiiipopazoglo@gmail.com>
2025-03-21 15:53:40 +0500

    Lesson 71. setState, local state

--- src/components/profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './ProfileInfo.module.css'
 import Preloader from '../../common/preloader/Preloader'
+import ProfileStatus from './ProfileStatus'
 
 function ProfileInfo(props) {
   if (!props.profile) {
@@ -9,13 +10,12 @@ function ProfileInfo(props) {
 
   return (
     <div>
-      <div>
+      {/* <div>
         <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" />
-      </div>
+      </div> */}
       <div className={styles.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        <h4>{props.profile.fullName}</h4>
-        <p>{props.profile.aboutMe}</p>
+        <ProfileStatus status={'hello'} />
       </div>
     </div>
   )
