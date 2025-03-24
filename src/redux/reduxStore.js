749b3083ff51dd7a3210160ce79b95b051fef3e6
SPopazoglo <sergiiipopazoglo@gmail.com>
2025-03-24 18:00:23 +0500

    Lesson 75. redux-form введение (login)

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -5,6 +5,7 @@ import dialogsReducer from './dialogsReducer'
 import sidebarReducer from './sidebarReducer'
 import usersReducer from './usersReducer'
 import authReducer from './authReducer'
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
   profilePage: profileReducer,
@@ -12,8 +13,11 @@ let reducers = combineReducers({
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 })
 
 let store = createStore(reducers, applyMiddleware(thunk))
 
 export default store
+
+window.store = store
